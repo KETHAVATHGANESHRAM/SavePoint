349755703ee9c508f02a3b9dd231aed6de153626
GANESH RAM <GANESH RAM@LAPTOP-TFP424JJ>
2024-09-09 11:21:03 +0530

    Initial commit

---
+++ JDBCSavePoint/src/com/codegnan/transactions/TransactionsDemo.java
@@ -0,0 +1,66 @@
+package com.codegnan.transactions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Statement;
+
+public class TransactionsDemo {
+	static final String JDBC_URL ="jdbc:mysql://localhost:3306/adjava";
+	static final String USERNAME = "root";
+	static final String PASSWORD= "root";
+	public static void main(String[] args) {
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			// establish connection
+			connection = DriverManager.getConnection(JDBC_URL,USERNAME,PASSWORD);
+			
+			// create the statement object
+			statement = connection.createStatement();
+			
+		
+			connection.setAutoCommit(false);
+			statement.executeUpdate("insert into politians values('Ganesh','BJP')");
+			statement.executeUpdate("insert into politians values('Ram','BRS')");
+			//create save point
+			Savepoint sp = connection.setSavepoint();
+			statement.executeUpdate("insert into politians values('Mani','TDP')");
+			System.out.println("oops....Wrong entry, rolling back ");
+			connection.rollback(sp);
+			System.out.println("rollback Successfull");
+			connection.commit();
+			System.out.println("Transaction Committed");
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			finally {
+				if(statement!=null) {
+					try {
+						statement.close();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(resultSet!=null) {
+					try {
+						resultSet.close();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				//scanner.close();
+			}
+	}
+}
